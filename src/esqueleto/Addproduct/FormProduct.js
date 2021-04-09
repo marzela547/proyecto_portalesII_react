aea208430862dd0ffa94fdd83622c8af93156591
Franciscogz <65878436+Franciscogz@users.noreply.github.com>
2021-04-09 15:27:27 -0600

    Agregar casi terminado

--- src/esqueleto/Addproduct/FormProduct.js
+++ src/esqueleto/Addproduct/FormProduct.js
@@ -1,7 +1,7 @@
 function Formu({ }) {
     return (
-        <form className="border-2 border-gray-500 relative my-3 mx-8 justify-center w-auto h-auto lg:w-2/8 lg:mt-5 flex lg:mr-5 lg:flex-wrap lg:align-middle ">
-            <fieldset className="border relative w-full h-full bg-pink-300 flex flex-wrap " >
+        <form className="  relative my-3 mx-8 justify-center w-auto h-auto lg:w-2/8 lg:mt-5 flex lg:mr-5 lg:flex-wrap lg:align-middle">
+            <fieldset className="border rounded-md  relative w-full h-full bg-red-200 flex flex-wrap " >
                 <div className="my-2 mx-2 h-12 w-full block lg:w-2/4 ">
                     <label for="txtproducto">Nombre del Producto</label>
                     <input type="text" name="txtproducto" id="txtproducto" className="w-full border border-gray-400"  placeholder="Anillo Fantasy" required />
@@ -9,9 +9,9 @@ function Formu({ }) {
                     <br/>
                 </div>
                 <div className="m-2 h-12 w-full flex p-0 lg:w-2/5 align-middle  ">
-                    <div className="ml-4">
+                    <div className=" w-2/4  ">
                         <label className="lbl" for="cmbmarca">Marca</label>
-                            <select name="cmbmarca" id="cmbmarca" required>
+                            <select name="cmbmarca" id="cmbmarca"  className="w-full" required>
                                 <option selected value="">Seleccionar...</option>
                                 <option value="opcpandora">Pandora</option>
                                 <option value="opccasio">Casio</option>
@@ -19,9 +19,9 @@ function Formu({ }) {
                             </select>
                         <br/>
                     </div>
-                    <div>
+                    <div className="ml-4 w-2/4  ">
                         <label className="lbl" for="cmbcategoria">Categoría</label>
-                            <select name="cmbcategoria" id="cmbcategoria" required>
+                            <select name="cmbcategoria" id="cmbcategoria" className="w-full" required>
                                 <option selected value="">Seleccionar...</option>
                                 <option value="opcpulseras">Pulseras</option>
                                 <option value="opcrelojes">Relojes</option>
@@ -36,8 +36,13 @@ function Formu({ }) {
                     <span id="txtprecioerror" class="hide"></span>
                     <br/>
                 </div>
-                <div className="flex  h-12 w-full lg:w-2/5 m-2">
-                  <div id="bexterior" className=" h-8 text-center align-middle flex border border-gray-400 bg-gray-500 w-full lg:h-12">
+                <div className="flex  h-12 w-full lg:w-72 m-2 border-gray-900 ">
+                  <div id="bexterior" className=" h-10 text-center align-middle flex border bg-gray-300  w-full lg:h-12">
+                  <div id="binterior" className="  lg:w-64 w-full m-1 lg:m-2  h-8 border-dashed border-gray-500 border border-collapse">
+                            <p className="text-xs mt-2 w-full overflow-hidden">Pulsa aquí para añadir una imagen</p>
+                            <input className="relative bottom-6 max-w-full z-0 opacity-0 " type="file" name="foto" id="foto" accept="image/png, image/jpg"/>
+                        </div>
+
                     </div>
                 </div>
                 <div  className="w-full h-32 m-2 mb-8">
@@ -45,7 +50,6 @@ function Formu({ }) {
                     <textarea id="txtLongText" nam e="txtLongText" rows="20" className="w-full h-full" placeholder="Descripción del producto" required></textarea>
                     <br/>
                 </div>
-
                 <div className="m-2 w-full h-12  align-middle flex  justify-end">
                     <button className="lg:w-28 lg:h-12 w-full h-full   bg-blue-600 text-blue-50 hover:text-blue-600 hover:bg-blue-50  " type="submit" name="btnEnviar">Guardar</button>
                 </div>
