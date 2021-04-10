a9a346465cdd7aa31e254968175cb648d88e3f09
MarlonMejia15 <66882055+MarlonMejia15@users.noreply.github.com>
2021-04-09 18:53:11 -0600

    Logincito
    Login

---
+++ src/esqueleto/login/login.js
@@ -0,0 +1,19 @@
+import '../login/login.css';
+
+function login() {
+    return (
+          <div class="login-box">
+            <img src="../Imgs/pandora-monograma.jpg" class="Persona" alt="Avatar" />
+            <h1>Bienvenido(a)</h1>
+            <br/>
+              <label for="username">Usuario</label>
+              <input type="text" placeholder="Enter Username"></input>
+              <label for="password">Contraseña</label>
+              <input type="password" placeholder="Enter Password"></input>
+              <input type="submit" value="Ingresar"></input>
+              <a href="#">Olvido su contraseña?</a>
+          </div>
+    );
+  }
+
+  export default login;
